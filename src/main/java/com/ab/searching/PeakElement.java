43add18069abf14472ecd752fcbf92034f5b4dd7
Arpit Bhardwaj <arpit.bhardwaj@sap.com>
2020-02-20 08:43:48 +0530

    Construct Tree

---
+++ src/main/java/com/ab/searching/PeakElement.java
@@ -0,0 +1,32 @@
+package com.ab.searching;
+
+/**
+ * @author Arpit Bhardwaj
+ *
+ * Given an array of integers. Find a peak element in it. An array element is peak if it is NOT smaller than its neighbors.
+ * Note that we need to return any one peak element.
+ */
+public class PeakElement {
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 20, 4, 1, 0};
+        int peakIndex = findPeakElementIndex(arr);
+        System.out.println("Peak index : " + peakIndex);
+    }
+
+    private static int findPeakElementIndex(int[] arr) {
+        return peakElementUtil(arr,0,arr.length-1);
+    }
+
+    private static int peakElementUtil(int[] arr, int low, int high) {
+        int mid = (low + high) >>> 1;
+
+        if ((mid == 0 || arr[mid -1] <= arr[mid])
+        && (mid == arr.length-1 || arr[mid + 1] <= arr[mid])){
+            return mid;
+        }
+        else if (mid > 0 && arr[mid-1] > arr[mid]){
+            return peakElementUtil(arr,low,mid - 1);
+        }
+        else return peakElementUtil(arr,mid + 1,high);
+    }
+}
---
+++ src/main/java/com/ab/trees/ConstructTree.java
@@ -0,0 +1,100 @@
+package com.ab.trees;
+
+/**
+ * @author Arpit Bhardwaj
+ *
+ * From Inorder and PreOrder
+ * From Inorder and LevelOrder
+ * From Inorder and PostOrder
+ *
+ * In a Preorder sequence, leftmost element is the root of the tree.
+ * In a Levelorder sequence, leftmost element is the root of the tree.
+ * In a Postorder sequence, rightmost element is the root of the tree.
+ */
+public class ConstructTree {
+
+    static int preIndex = 0;//for indexing preOrder array
+    //static int postIndex = 0;//for indexing postOrder array
+
+    public static void main(String[] args) {
+        /*Character[] preOrder = {'A', 'B', 'D', 'E', 'C', 'F'};
+        Character[] inOrder = {'D', 'B', 'E', 'A', 'F', 'C'};
+        Character[] postOrder = {'D', 'B', 'E', 'A', 'F', 'C'};*/
+
+        //Integer[] preOrder = {1, 2, 4, 5, 3, 6};
+        Integer[] inOrder = {4, 2, 5, 1, 6, 3};
+        Integer[] postOrder = {4, 5, 2, 6, 3, 1};
+
+        /*Integer[] inOrder = {4, 8, 2, 5, 1, 6, 3, 7};
+        Integer[] postOrder = {8, 4, 5, 2, 6, 7, 3, 1};*/
+
+        //postIndex = postOrder.length - 1;
+        ConstructTree constructTree = new ConstructTree();
+
+        //GenericNode<Character> root = constructTree.constructTreeFromInOrderPreOrder(inOrder, preOrder, 0, inOrder.length - 1);
+        //GenericNode<Integer> root = constructTree.constructTreeFromInOrderPreOrder(inOrder,preOrder,0,inOrder.length - 1);
+        Index pIndex = new Index();
+        pIndex.index = postOrder.length - 1;
+
+        GenericNode<Integer> root = constructTree.constructTreeFromInOrderPostOrder(inOrder, postOrder, 0, inOrder.length - 1, pIndex);
+        System.out.println(root);
+    }
+
+    private <T> GenericNode<T> constructTreeFromInOrderPreOrder(T[] inOrder, T[] preOrder, int start, int end) {
+        if(start > end){
+            return null;
+        }
+
+        //take the node one by one from preOrder
+        GenericNode<T> root = new GenericNode(preOrder[preIndex++]);
+
+        //base condition for leaf nodes
+        if(start == end){
+            return root;
+        }
+
+        int inIndex = this.getInorderIndex(inOrder, start, end, root.data);//for indexing inOrder array
+
+        root.left = constructTreeFromInOrderPreOrder(inOrder,preOrder,start,inIndex-1);
+        root.right = constructTreeFromInOrderPreOrder(inOrder,preOrder,inIndex+1,end);
+
+        return root;
+    }
+
+    private <T> GenericNode<T> constructTreeFromInOrderPostOrder(T[] inOrder, T[] postOrder, int start, int end,Index pIndex) {
+        if(start > end){
+            return null;
+        }
+
+        //take the node one by one from preOrder
+        GenericNode<T> root = new GenericNode(postOrder[pIndex.index]);
+        (pIndex.index)--;
+        //base condition for leaf nodes
+        if(start == end){
+            return root;
+        }
+
+        int inIndex = this.getInorderIndex(inOrder, start, end, root.data);//for indexing inOrder array
+
+        root.right = constructTreeFromInOrderPostOrder(inOrder,postOrder,inIndex+1,end,pIndex);
+        root.left = constructTreeFromInOrderPostOrder(inOrder,postOrder,start,inIndex-1,pIndex);
+
+        return root;
+    }
+
+    private <T> int getInorderIndex(T[] arr, int start, int end, T value) {
+        //search the node in inOrder array
+        int inIndex = 0;
+        for (int i = start; i <= end; i++) {
+            if(value == arr[i]){
+                inIndex = i;
+            }
+        }
+        return inIndex;
+    }
+
+    // Class Index created to implement pass by reference of Index
+    static class Index {
+        int index;
+    }
+}
